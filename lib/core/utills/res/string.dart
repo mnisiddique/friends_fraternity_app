28f054b3dee4df547a82c753fda8fc374c0b14d3
Prince <mnisiddique30@gmail.com>
2022-05-21 20:05:25 +0600

    Adds address

--- lib/core/utills/res/string.dart
+++ lib/core/utills/res/string.dart
@@ -2,6 +2,7 @@ class StringRes {
   StringRes._init();
   static const String title = "বন্ধু পরিষদ";
   static const String moto = "শ্রেনিহীন সমাজ প্রতিষ্ঠার দুরন্ত সাহস";
-  static const String address =
-      "পন্থাবাড়ি, সুখাতি ডাকঘর, নেওয়াশী, নাগেশ্বরী, কুড়িগ্রাম";
+  static const String addressLine1 = "পন্থাবাড়ি, সুখাতি ডাকঘর";
+  static const String addressLine2 = ", নেওয়াশী, নাগেশ্বরী, কুড়িগ্রাম";
+  static const String version = "সংস্করণ ১.০.১";
 }
