e6c863182174238c8560a5dd9fb7aef274c9893b
Prince <mnisiddique30@gmail.com>
2022-05-21 17:21:39 +0600

    Adds footer text

--- lib/core/utills/res/string.dart
+++ lib/core/utills/res/string.dart
@@ -1,5 +1,7 @@
 class StringRes {
   StringRes._init();
-  static const title = "বন্ধু পরিষদ";
-  static const moto = "শ্রেনিহীন সমাজ প্রতিষ্ঠার দুরন্ত সাহস";
+  static const String title = "বন্ধু পরিষদ";
+  static const String moto = "শ্রেনিহীন সমাজ প্রতিষ্ঠার দুরন্ত সাহস";
+  static const String address =
+      "পন্থাবাড়ি, সুখাতি ডাকঘর, নেওয়াশী, নাগেশ্বরী, কুড়িগ্রাম";
 }
