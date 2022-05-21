58e3b332bac1d397fe24f852fe86954f3534f1a4
Prince <mnisiddique30@gmail.com>
2022-05-21 17:23:12 +0600

    Adds footer text style

---
+++ lib/core/utills/res/style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:friends_fraternity_app/core/utills/res/resource.dart';
+
+class StyleRes {
+  StyleRes._init();
+  static const TextStyle footerTextStyle = const TextStyle(
+    fontSize: 12.0,
+    color: ColorRes.primaryMaterialColor,
+    fontWeight: FontWeight.normal,
+  );
+}
