4198279267116a946847ba89d94392f597b89847
Prince <mnisiddique30@gmail.com>
2022-05-29 02:09:36 +0600

    Add files for depositor use cases

--- lib/core/utills/dependency.dart
+++ lib/core/utills/dependency.dart
@@ -1,4 +1,6 @@
 import 'package:friends_fraternity_app/core/network/network_info.dart';
+import 'package:friends_fraternity_app/data/data_source/datasource_exporter.dart';
+import 'package:friends_fraternity_app/data/repo_impl/repo_impl_exporter.dart';
 import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 
@@ -7,6 +9,17 @@ final GetIt sl = GetIt.instance;
 class Dependency {
   Dependency._init();
   static void initDependency() {
+    sl.registerLazySingleton<DepositorHistoryRemoteImpl>(
+      () => DepositorHistoryRemoteImpl(),
+    );
+
+    sl.registerLazySingleton<DepositHistoryRepoImpl>(
+      () => DepositHistoryRepoImpl(
+        depositorHistoryRemote: sl(),
+        networkInfo: sl(),
+      ),
+    );
+
     //Libraries
     sl.registerLazySingleton<NetworkInfoImpl>(
       () => NetworkInfoImpl(
