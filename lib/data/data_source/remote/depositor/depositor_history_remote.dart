b64bbb12f3ac41af4d04ffb937102cbab710a3e4
Prince <mnisiddique30@gmail.com>
2022-05-28 09:40:48 +0600

    Adds param to depositor history remote

--- lib/data/data_source/remote/depositor/depositor_history_remote.dart
+++ lib/data/data_source/remote/depositor/depositor_history_remote.dart
@@ -1,12 +1,13 @@
+import 'package:friends_fraternity_app/core/params/param_exporter.dart';
 import 'package:friends_fraternity_app/domain/entity/entity_exporter.dart';
 
 abstract class DepositorHistoryRemote {
-  Future<List<EachMonthDeposit>> call();
+  Future<List<EachMonthDeposit>> call(Params params);
 }
 
 class DepositorHistoryRemoteImpl implements DepositorHistoryRemote {
   @override
-  Future<List<EachMonthDeposit>> call() {
+  Future<List<EachMonthDeposit>> call(Params param) {
     // TODO: implement call
     throw UnimplementedError();
   }
