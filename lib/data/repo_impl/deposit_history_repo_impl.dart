079e77bd8b757a7611b7ffb9612c71b49e35e5a1
Prince <mnisiddique30@gmail.com>
2022-05-29 01:38:12 +0600

    Changes return type

--- lib/data/repo_impl/deposit_history_repo_impl.dart
+++ lib/data/repo_impl/deposit_history_repo_impl.dart
@@ -1,6 +1,7 @@
 import 'package:friends_fraternity_app/core/network/network_info.dart';
 import 'package:friends_fraternity_app/core/params/param_exporter.dart';
 import 'package:friends_fraternity_app/data/data_source/datasource_exporter.dart';
+import 'package:friends_fraternity_app/data/model/depositor/each_month_deposit_model.dart';
 import 'package:friends_fraternity_app/domain/entity/entity_exporter.dart';
 import 'package:friends_fraternity_app/domain/repository/repository.dart';
 
@@ -14,7 +15,8 @@ class DepositHistoryRepoImpl implements DepositHistoryRepo {
   });
 
   @override
-  Future<List<EachMonthDeposit>> getPersonDepositHistory(Params param) async {
+  Future<EachMonthDepositListModel> getPersonDepositHistory(
+      Params param) async {
     await networkInfo.throwIfNoInternet();
     return await depositorHistoryRemote(param);
   }
