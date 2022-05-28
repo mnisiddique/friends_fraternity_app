b72e901dcb290153ed70866c335229145209bd61
Prince <mnisiddique30@gmail.com>
2022-05-28 09:44:28 +0600

    Throws no internet exception while fetching depositor history

--- lib/data/repo_impl/deposit_history_repo_impl.dart
+++ lib/data/repo_impl/deposit_history_repo_impl.dart
@@ -1,15 +1,21 @@
+import 'package:friends_fraternity_app/core/network/network_info.dart';
 import 'package:friends_fraternity_app/core/params/param_exporter.dart';
 import 'package:friends_fraternity_app/data/data_source/datasource_exporter.dart';
 import 'package:friends_fraternity_app/domain/entity/entity_exporter.dart';
 import 'package:friends_fraternity_app/domain/repository/repository.dart';
 
 class DepositHistoryRepoImpl implements DepositHistoryRepo {
-  final DepositorHistoryRemote _depositorHistoryRemote;
+  final DepositorHistoryRemote depositorHistoryRemote;
+  final NetworkInfo noInternetExceptionThrower;
 
-  DepositHistoryRepoImpl(this._depositorHistoryRemote);
+  DepositHistoryRepoImpl({
+    required this.depositorHistoryRemote,
+    required this.noInternetExceptionThrower,
+  });
 
   @override
   Future<List<EachMonthDeposit>> getPersonDepositHistory(Params param) async {
-    return await _depositorHistoryRemote();
+    await noInternetExceptionThrower.throwIfNoInternet();
+    return await depositorHistoryRemote(param);
   }
 }
