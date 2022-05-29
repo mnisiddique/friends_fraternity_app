4ad5f1e2d33a5a15e1f506bc216722bed28ec2fb
Prince <mnisiddique30@gmail.com>
2022-05-30 01:11:19 +0600

    Exports depostor data sources

--- lib/data/data_source/export_datasrc.dart
+++ lib/data/data_source/export_datasrc.dart
@@ -1,1 +1,5 @@
 export "./remote/depositor/depositor_history_remote.dart";
+export "./remote/depositor/depositorship_observation_remote/depositor_list_remote.dart";
+export "./remote/depositor/depositorship_observation_remote/depositorship_cancel_remote.dart";
+export "./local/depositor/depositorship_observation_local/depositor_list_local.dart";
+export "./local/depositor/depositorship_observation_local/depositorship_cancel_local.dart";
