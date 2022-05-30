563ae00086bf69b4503510e4bd283d2ee38952ed
Prince <mnisiddique30@gmail.com>
2022-05-31 01:24:18 +0600

    exports timeline calculation source

---
+++ lib/core/calculation_source/export_core_calculation_source.dart
@@ -0,0 +1,1 @@
+export 'package:friends_fraternity_app/core/calculation_source/timeline_calculation_source.dart';
--- lib/core/calculation_source/timeline_calculation_src.dart
+++
@@ -1,6 +0,0 @@
-import 'package:friends_fraternity_app/core/params/export_param.dart';
-
-abstract class TimelineObserverRepo {
-  int workoutDueMonth(Params params);
-  int workoutAdvanceMonth(Params params);
-}
