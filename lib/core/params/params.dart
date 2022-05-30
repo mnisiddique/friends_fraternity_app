31d7b59f9772d4bcecad732d37d00ea03c2f5852
Prince <mnisiddique30@gmail.com>
2022-05-31 00:14:57 +0600

    Adds timeline param

--- lib/core/params/params.dart
+++ lib/core/params/params.dart
@@ -1,3 +1,9 @@
 abstract class Params {}
 
+abstract class TimelineParam extends Params {
+  String get firstDate;
+  String get lastDate;
+  num get totalAmount;
+}
+
 class NoParams implements Params {}
