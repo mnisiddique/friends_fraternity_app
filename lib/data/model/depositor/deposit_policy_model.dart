fd3acfa0431435fffc8dc0c25bc5188b9d338ded
Prince <mnisiddique30@gmail.com>
2022-06-03 00:36:06 +0600

    Exports deposit policy model

---
+++ lib/data/model/depositor/deposit_policy_model.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:friends_fraternity_app/domain/entity/export_entity.dart';
+
+class DepositPolicyModel extends DepositPolicy {
+  DepositPolicyModel({
+    required super.amount,
+    required super.penalty,
+    required super.depositorshipRevocationTimeLimit,
+  });
+
+  factory DepositPolicyModel.fromDepositPolicy(DepositPolicy model) {
+    return DepositPolicyModel(
+      amount: model.amount,
+      penalty: model.penalty,
+      depositorshipRevocationTimeLimit: model.depositorshipRevocationTimeLimit,
+    );
+  }
+
+  factory DepositPolicyModel.fromJson(String jsonResp) {
+    final decodedJson = json.decode(jsonResp);
+    return DepositPolicyModel(
+      amount: decodedJson["amount"],
+      penalty: decodedJson["penalty"],
+      depositorshipRevocationTimeLimit:
+          decodedJson["depositorshipRevocationTimeLimit"],
+    );
+  }
+
+  String toJson() {
+    return json.encode({
+      "amount": amount,
+      "penalty": penalty,
+      "depositorshipRevocationTimeLimit": depositorshipRevocationTimeLimit,
+    });
+  }
+
+  static String toHardCodedJson() {
+    return json.encode({
+      "amount": 1000,
+      "penalty": 200,
+      "depositorshipRevocationTimeLimit": 3,
+    });
+  }
+}
--- lib/data/model/model.dart
+++ lib/data/model/model.dart
@@ -1,3 +1,4 @@
 export "depositor/depositor_model.dart";
 export "depositor/depositor_cancellation_status_model.dart";
 export "depositor/each_month_deposit_model.dart";
+export "depositor/deposit_policy_model.dart";
