a10972c60a0edc8e62916276f53be93b3b55647a
Prince <mnisiddique30@gmail.com>
2022-06-02 23:03:54 +0600

    Completes Timeline counting logic roughly

--- lib/data/repo_impl/depositor/depositor_timeline_observer_repo_impl.dart
+++ lib/data/repo_impl/depositor/depositor_timeline_observer_repo_impl.dart
@@ -1,4 +1,6 @@
-import 'package:friends_fraternity_app/core/utills/date_time/date_interval_counter.dart';
+import 'package:friends_fraternity_app/core/calculation_source/export_core_counting_source.dart';
+import 'package:friends_fraternity_app/core/local_datasource/shared_preference/current_date_local_source.dart';
+import 'package:friends_fraternity_app/core/model/export_core_model.dart';
 import 'package:friends_fraternity_app/data/model/model.dart';
 import 'package:friends_fraternity_app/domain/entity/depositor/depositor_entities.dart';
 import 'package:friends_fraternity_app/core/params/params.dart';
@@ -6,34 +8,45 @@ import 'package:friends_fraternity_app/domain/repository/export_repo.dart';
 
 class DepositorTimelineObserverRepoImpl
     implements DepositorTimelineObserverRepo {
+  final TimelineCountingSourceSourceImpl timelineCountingSrc;
+  final CurrentDateLocalSource localDateSrc;
+
+  DepositorTimelineObserverRepoImpl({
+    required this.timelineCountingSrc,
+    required this.localDateSrc,
+  });
+
   @override
   DepositorshipObservation observeDepositorTimeline(Params param) {
     final DepositorListModel model = param as DepositorListModel;
     List<Depositor> depositorsObserved = [];
     List<String> depositorCancelled = [];
 
-    model.depositors.forEach(
-      (depositorItem) {
-        final int dMonths = 0;
-        // final int dMonths = workoutDueMonth(depositorItem);
-        final bool active = dMonths >= 3;
-        depositorsObserved.add(
-          depositorItem.copyWith(
-            dueMonths: dMonths,
-            monthsDepositInAdvance: 0,
-            // monthsDepositInAdvance: workoutAdvanceMonth(depositorItem),
-            isActive: active,
-          ),
-        );
-        if (active) {
-          depositorCancelled.add(depositorItem.nid);
-        }
-      },
-    );
-
+    model.depositors.forEach((depositorItem) {
+      depositorsObserved.add(observe(depositorItem));
+      if (!depositorItem.isActive) {
+        depositorCancelled.add(depositorItem.nid);
+      }
+    });
     return DepositorshipObservation(
       observedDepositors: depositorsObserved,
       cancelledDepositors: depositorCancelled,
     );
+  }
+
+  DepositorModel observe(DepositorModel depositor) {
+    final timeline = TimelineModel(
+      firstDate: depositor.firstDepositDate,
+      lastDate: depositor.lastDepositDate,
+      currentDate: localDateSrc.getCurrentDate(),
+      timelineInMonth: 3,
+      monthlyPayment: 0,
+      totalPayedAmount: depositor.totalDeposit,
+    );
+    final dueMonth = timelineCountingSrc.countDueMonth(timeline);
+    return depositor.copyWith(
+        dueMonths: dueMonth,
+        monthsDepositInAdvance: timelineCountingSrc.countAdvanceMonth(timeline),
+        isActive: dueMonth >= 3);
   }
 }
