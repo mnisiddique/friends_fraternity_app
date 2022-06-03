32743bc34220c1283d5b358c7803172aef3e982c
Prince <mnisiddique30@gmail.com>
2022-06-03 21:29:27 +0600

    Refactor export statement

---
+++ lib/data/export_model.dart
@@ -0,0 +1,4 @@
+export 'package:friends_fraternity_app/data/model/depositor/depositor_model.dart';
+export 'package:friends_fraternity_app/data/model/depositor/depositor_cancellation_status_model.dart';
+export 'package:friends_fraternity_app/data/model/depositor/each_month_deposit_model.dart';
+export 'package:friends_fraternity_app/data/model/depositor/deposit_policy_model.dart';
---
+++ lib/data/export_repo_impl.dart
@@ -0,0 +1,7 @@
+//Common repo impls
+
+//Specific
+export 'package:friends_fraternity_app/data/repo_impl/depositor/deposit_history_repo_impl.dart';
+export 'package:friends_fraternity_app/data/repo_impl/depositor/depositor_repo_impl.dart';
+export 'package:friends_fraternity_app/data/repo_impl/depositor/depositor_timeline_observer_repo_impl.dart';
+export 'package:friends_fraternity_app/data/repo_impl/depositor/deposit_policy_repo_impl.dart';
