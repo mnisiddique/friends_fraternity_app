3934f8e0fa36f438657cdc4dfb8be14fbfe689d5
Prince <mnisiddique30@gmail.com>
2022-06-21 02:21:08 +0600

    Updates use case signature

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -1,7 +1,10 @@
-abstract class BaseUseCase<T, Params> {
+import 'package:dartz/dartz.dart';
+import 'package:friends_fraternity_app/core/error/export_error.dart';
+
+abstract class Usecase<T, Params> {
   T call(Params params);
 }
 
-abstract class BaseUseCaseAsync<T, Params> {
-  Future<T> call(Params params);
+abstract class AsyncUsecase<T, Params> {
+  Future<Either<Failure, T>> call(Params params);
 }
--- lib/core/utills/dependency.dart
+++ lib/core/utills/dependency.dart
@@ -1,6 +1,7 @@
 import 'package:friends_fraternity_app/core/network/network_info.dart';
 import 'package:friends_fraternity_app/data/export_datasrc.dart';
 import 'package:friends_fraternity_app/data/export_repo_impl.dart';
+import 'package:friends_fraternity_app/domain/export_repository.dart';
 import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 
@@ -9,11 +10,11 @@ final GetIt sl = GetIt.instance;
 class Dependency {
   Dependency._init();
   static void initDependency() {
-    sl.registerLazySingleton<DepositorHistoryRemoteImpl>(
+    sl.registerLazySingleton<DepositHistoryRemote>(
       () => DepositorHistoryRemoteImpl(),
     );
 
-    sl.registerLazySingleton<DepositHistoryRepoImpl>(
+    sl.registerLazySingleton<DepositHistoryRepo>(
       () => DepositHistoryRepoImpl(
         depositHistoryRemote: sl(),
         networkInfo: sl(),
@@ -21,7 +22,7 @@ class Dependency {
     );
 
     //Libraries
-    sl.registerLazySingleton<NetworkInfoImpl>(
+    sl.registerLazySingleton<NetworkInfo>(
       () => NetworkInfoImpl(
         InternetConnectionChecker(),
       ),
